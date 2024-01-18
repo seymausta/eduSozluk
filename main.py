2b41bfbaad91120e2fc9cb364922ff6bda597b6b
Şeymanur Usta <seymanur.usta.123@gmail.com>
2024-01-18 21:31:06 +0300

    04.01.2023 dosya'dan çekme

--- main.py
+++ main.py
@@ -1,44 +1,32 @@
-from flask import Flask, render_template #render_template html şablonlarını render etmek için kullanılır
+from flask import Flask, render_template  # render_template html şablonlarını render etmek için kullanılır
 
-app = Flask('app') #flask uygulaması oluşturuluyor, app parametresi uygulamanın adı
+app = Flask('app')  # flask uygulaması oluşturuluyor, app parametresi uygulamanın adı
 
-#veri kısmı
-basliklar = [{
-  "id": 1,
-  "baslik": "Yılbaşı film önerileri",
-  "icerikler": ["icerik1", "icerik2", "icerik3"]},
-  {
-    "id": 2,
-    "baslik": "2024 yılbaşı mesajları",
-    "icerikler": ["icerik4", "icerik5", "icerik6"]},
-  {
-    "id": 3,
-    "baslik": "2024'te ücretelere gelecek zamlar",
-    "icerikler": ["icerik7", "icerik8", "icerik9"]},
-  {
-    "id": 4,
-    "baslik": "python ne işe yarar",
-    "icerikler": ["icerik10", "icerik11", "icerik12"]},
-  {
-    "id": 5,
-    "baslik": "bye bye 2023",
-    "icerikler": ["icerik13", "icerik14", "icerik15"]},
-]
+# veri kısmı
+basliklar = []
 
-@app.route('/')  #anasayfa için bir root tanımlar
+
+@app.route('/')  # anasayfa için bir root tanımlar
 def home_page():
-  return render_template("home_page.html",basliklar=basliklar) #belirli bir başlığın içeriği görüntülenir
+    dosya = open("./sozluk_dosyalar/basliklar.txt", "r", encoding="utf-8")
+    dosyadan_basliklar = dosya.readlines()
+    basliklar.clear()
 
-@app.route('/baslik/<baslik_id>') #baslik_id parametresi ile belirlenen bir başlık sayfası için bir route tanımlar
-def baslik_goster(baslik_id):
-  baslik=""
-  icerikler=[]
-  for b in basliklar:
-    if b["id"]==int(baslik_id):
-      baslik=b["baslik"]
-      icerikler=b["icerikler"]
+    for b in dosyadan_basliklar:
+        parcalar = b.split("|")
+        basliklar.append({
+            "id": parcalar[0],
+            "baslik": parcalar[1]}
+        )
+    return render_template('home_page.html', basliklar=basliklar, baslik="Anasayfa")  # belirli bir başlığın içeriği görüntülenir
 
-  return render_template("baslik_icerik.html",baslik=baslik,icerikler=icerikler)
+
+@app.route('/baslik/<baslik_id>/<baslik_metin>')
+def baslik_goster(baslik_id, baslik_metin):
+    dosya_adi = baslik_id + ".txt"
+    dosya = open(f"./sozluk_dosyalar/{dosya_adi}", "r", encoding="utf-8")
+    dosyadan_yazilar = dosya.readlines()
+    return render_template("baslik_icerik.html", baslik=baslik_metin, icerikler=dosyadan_yazilar, baslik_id=baslik_id)
 
 
 app.run(debug=True, host='0.0.0.0', port=5000)
