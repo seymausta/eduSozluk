90083a194dac869c1280bda15d6ba5da209915b9
Şeymanur Usta <seymanur.usta.123@gmail.com>
2024-01-18 22:40:24 +0300

    18.01.2023 Veritabanı

--- main.py
+++ main.py
@@ -1,39 +1,44 @@
 from flask import Flask, render_template, request,redirect  # render_template html şablonlarını render etmek için kullanılır
+import pymongo
 
 app = Flask('app')  # flask uygulaması oluşturuluyor, app parametresi uygulamanın adı
 
 # veri kısmı
 basliklar = []
 
+myclient = pymongo.MongoClient("mongodb://localhost:27017/")
+mydb = myclient["eduSozlukDB"]
 
 @app.route('/')  # anasayfa için bir root tanımlar
 def home_page():
-    dosya = open("./sozluk_dosyalar/basliklar.txt", "r", encoding="utf-8")
-    dosyadan_basliklar = dosya.readlines()
-    basliklar.clear()
-
-    for b in dosyadan_basliklar:
-        parcalar = b.split("|")
-        basliklar.append({
-            "id": parcalar[0],
-            "baslik": parcalar[1]}
-        )
+   # dosya = open("./sozluk_dosyalar/basliklar.txt", "r", encoding="utf-8")
+   # dosyadan_basliklar = dosya.readlines()
+   # basliklar.clear()
+    mycol = mydb["basliklar"]
+    basliklar = list(mycol.find({}))
     return render_template('home_page.html', basliklar=basliklar, baslik="Anasayfa")  # belirli bir başlığın içeriği görüntülenir
 
 
-@app.route('/baslik/<baslik_id>/<baslik_metin>')
-def baslik_goster(baslik_id, baslik_metin):
-    dosya_adi = baslik_id + ".txt"
-    dosya = open(f"./sozluk_dosyalar/{dosya_adi}", "r", encoding="utf-8")
-    dosyadan_yazilar = dosya.readlines()
-    return render_template("baslik_icerik.html", baslik=baslik_metin, icerikler=dosyadan_yazilar, baslik_id=baslik_id)
+@app.route('/baslik/<baslik_id>')
+def baslik_goster(baslik_id):
+    mycol = mydb["yazilar"]
+    veritabanından_yazilar = list(mycol.find({"baslik_id":int(baslik_id)}))
+    baslik_tablosu=mydb["basliklar"]
+    baslik_kaydi=baslik_tablosu.find_one({"_id":int(baslik_id)})
+    baslik_metin=baslik_kaydi["baslik"]
+    return render_template("baslik_icerik.html", baslik=baslik_metin, yazilar=veritabanından_yazilar, baslik_id=baslik_id)
 
 @app.route('/yazi-ekle', methods=['POST'])
 def yazi_ekle():
     baslik_id=request.form.get('txtBaslikId')
     yazi=request.form.get('txtYazi')
-    dosya = open(f"./sozluk_dosyalar/{baslik_id}.txt", "a", encoding="utf-8")
-    dosya.write("\n" + yazi)
-    return redirect("/",302)
+
+    mycol = mydb["yazilar"]
+    yeni_yazi={"baslik_id":int(baslik_id),"yazi":yazi}
+    x=mycol.insert_one(yeni_yazi)
+
+    #dosya = open(f"./sozluk_dosyalar/{baslik_id}.txt", "a", encoding="utf-8")
+    #dosya.write("\n" + yazi)
+    return redirect("/baslik/"+baslik_id,302)
 
 app.run(debug=True, host='0.0.0.0', port=5000)
